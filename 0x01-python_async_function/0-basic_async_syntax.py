b13a119ed25c90948ac0681ab76242dfe4cf3db2
Joshua <atsujoshua67@gmail.com>
2024-10-14 10:55:13 -0700

    The basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import random
 """
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     takes in an integer argument
     waits for a random delay between 0 and max_delay seconds
