6c41a8a49a88309af6b791d89817dc17e684e3f2
Joshua <atsujoshua67@gmail.com>
2024-10-16 16:11:02 -0700

    type-annotation

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 7 Module
+"""
+from typing import Tuple Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Complex types - string and int/float to tuple
+    """
+    return k v**2
