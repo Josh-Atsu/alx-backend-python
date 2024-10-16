22c6b0b70ac5cf9d1320957c6eac17b1a46a8863
Joshua <atsujoshua67@gmail.com>
2024-10-16 11:47:22 -0700

    type-annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Task 5's Module
 """
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Complex types - list of floats
     """
     return sum(input_list)
