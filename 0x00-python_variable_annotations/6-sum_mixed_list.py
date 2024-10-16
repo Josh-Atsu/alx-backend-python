cd409764e18b667bfaf5f664b9053f6194629117
Joshua <atsujoshua67@gmail.com>
2024-10-16 12:05:36 -0700

    type-annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Task 6 Module
 """
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Complex types - mixed list
     """
     return sum(mxd_lst)
